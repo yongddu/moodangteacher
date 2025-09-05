f70a32aa802c45c972a67313da198e2df60a85d2
yongsunny <kweon105@naver.com>
2025-09-05 15:35:14 +0900

    9월5일 수정중

--- js/common.js
+++ js/common.js
@@ -1,23 +1,37 @@
-document.addEventListener("DOMContentLoaded",() => {
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
+document.addEventListener("DOMContentLoaded", () => {
+  // 비주얼 슬라이더
+  const teamSlider = new Swiper(".team-slider", {
+    // 무한 루프
+    loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
+    // 자동 재생
+    autoplay: {
+      delay: 3, // 3초마다 자동 슬라이드
+      disableOnInteraction: false, // 사용자 상호작용 후에도 자동재생 계속
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-});
+
+    // 페이지네이션
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true, // 페이지네이션 클릭 가능
+    },
+
+    // breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 3,
+      },
+    },
+  });
 });
